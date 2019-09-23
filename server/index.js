a28f7b70cf9b36b2d6624f37a324a7a8ab609834
James Lee <james.k.lee4@gmail.com>
2019-09-22 21:17:19 -0700

    Initialize node server

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const lobRouter = require('./routers/lob.js');
+
+const app = express();
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  next();
+});
+
+app.use('/', lobRouter);
+
+const port = 3000
+app.listen(port, () => console.log('LOB Service listening on port', port));
