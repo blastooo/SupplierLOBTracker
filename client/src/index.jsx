c15a0a3c610af8eff788399e7b5648884b456adf
James Lee <james.k.lee4@gmail.com>
2019-09-25 18:49:15 -0700

    Update client to run initial data pull from server

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -6,25 +6,44 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      supplier: null,
+      contracts: null
     };
+    this.supplierId = 1;
   };
 
   componentDidMount() {
+    this.getSupplier();
+    this.getContracts();
   };
 
   // GET request to retrieve Supplier info
   getSupplier() {
+    $.get('/lob/supplier/' + this.supplierId, (supplierInfo) => {
+      this.setState({
+        supplier: supplierInfo
+      });
+    });
   };
 
   // GET request to retrieve Contracts data
   getContracts() {
+    $.get('/lob/supplier/' + this.supplierId + '/contracts', (contractsData) => {
+      this.setState({
+        contracts: contractsData
+      });
+    });
   };
 
   render() {
     return (
-      <div>
-        Line of Balance
-      </div>
+      (this.state.supplier === null || this.state.contracts === null) ?
+        // Display loading screen upon initial render
+        <img src="https://i.imgur.com/k9GyXLC.gif" />
+      :
+        <div>
+          Line of Balance
+        </div>
     );
   };
 };
