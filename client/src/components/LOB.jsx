fe3cc9089a5f62e8b56d2c10b5ba8d70c4bba15c
James Lee <james.k.lee4@gmail.com>
2019-10-05 09:30:16 -0700

    Client: Hook up DemandMatrixByWeek child component to parent LOB component

--- client/src/components/LOB.jsx
+++ client/src/components/LOB.jsx
@@ -1,27 +1,92 @@
 import React from 'react';
 import moment from 'moment';
 
-const LOB = ({contracts}) => {
+import DemandMatrixByLN from './DemandMatrixByLN.jsx';
 
-  // Used to generate table header for Line Numbers
-  const lineNumbers = [];
-  if (!!contracts) {
-    const start = contracts[0].demand[0].lineNumber;
-    for (let i = start; i <= start + 30; i++) {
-      lineNumbers.push(i);
+class LOB extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      demandType: 'Line Number'
+    };
+  };
+
+  // Change demand matrix to display by Line Number, Week, or Month
+  changeDemandType(event) {
+    this.setState({
+      demandType: event.target.value
+    });
+  };
+
+  render() {
+    // Used to generate table header for demand matrix based on demand type
+    const demandHeader = [];
+    let start = null;
+    let end = null;
+    let dateType = null;
+    let dateFormat = null;
+    let demandMatrix = null;
+    let topHeader = [];
+    let headerLength = null;
+    let index = -1;
+
+    // Generate table header for demand matrix
+    if (!!this.props.contracts) {
+
+      if (this.state.demandType === 'Line Number') {
+        topHeader.push({'Line Number': 31});
+        start = this.props.contracts[0].demand[0].lineNumber;
+        end = start + 31;
+        demandMatrix = <DemandMatrixByLN contracts={this.props.contracts} headers={demandHeader} />;
+      } else {
+        if (this.state.demandType === 'Weekly') {
+          dateType = 'weeks';
+          dateFormat = 'YYYY-MM-DD';
+          headerLength = 28;
+          // demandMatrix = <DemandMatrixByWeek contracts={this.props.contracts} headers={demandHeader} />;
+        } else {
+          dateType = 'months';
+          dateFormat = 'YYYY-MM-01';
+          headerLength = 16;
+          // demandMatrix = <DemandMatrixByMonth contracts={this.props.contracts} headers={demandHeader} />;
+        }
+        start = this.props.contracts.reduce((min, contract) =>
+          contract.demand[0].needDate < min ? contract.demand[0].needDate : min
+        );
+        start = moment(start);
+        end = moment(start).add(headerLength, dateType);
+      }
+
+      while (start < end) {
+        if (typeof start === 'number') {
+          demandHeader.push(start);
+          start++;
+        } else {
+          demandHeader.push(moment(start).format(dateFormat));
+
+          let currentYear = moment(start).format('YYYY');
+          if (topHeader.length && Object.keys(topHeader[index])[0] === currentYear) {
+            topHeader[index][currentYear]++;
+          } else {
+            let yearObject = {};
+            yearObject[currentYear] = 1;
+            topHeader[++index] = yearObject;
+          }
+
+          start = moment(start).add(1, dateType);
+        }
+      }
     }
-  }
 
-  // Used for visualizing current date line
-  let previousDateLine = null;
-  let currentDateLine = null;
-
-
-  return (
-    !!contracts &&
+    return (
+      !!this.props.contracts &&
       <div>
 
         <h3> Line of Balance </h3>
+
+        <label><input type="radio" value="Line Number" checked={this.state.demandType === 'Line Number'} onChange={this.changeDemandType.bind(this)} /> Line Number </label>
+        <label><input type="radio" value="Weekly" checked={this.state.demandType === 'Weekly'} onChange={this.changeDemandType.bind(this)} /> Weekly </label>
+        <label><input type="radio" value="Monthly" checked={this.state.demandType === 'Monthly'} onChange={this.changeDemandType.bind(this)} /> Monthly </label>
 
         <table>
 
@@ -33,90 +98,30 @@ const LOB = ({contracts}) => {
               <th rowSpan="2"> Nomenclature </th>
               <th rowSpan="2"> WIP </th>
               <th rowSpan="2"> Inventory </th>
-              <th colSpan="31"> Line Number </th>
+              {topHeader.map((val, index) =>
+                <th key={index} colSpan={val[Object.keys(val)[0]]}> {Object.keys(val)[0]} </th>
+              )}
             </tr>
             <tr>
-              {lineNumbers.map((line, index) =>
-                <th key={index}> {line} </th>
-              )}
+              {demandHeader.map((val, index) => {
+                let value = val;
+                if (dateType === 'weeks') {
+                  value = moment(value).format('M/D');
+                } else if (dateType === 'months') {
+                  value = moment(value).format('MMM');
+                }
+                return <th key={index}> {value} </th>
+              })}
             </tr>
           </thead>
 
-          <tbody>
-            {contracts.map((contract, index) => {
-              let riskStoplight = {
-                'position': 'relative',
-                'height': 'auto%',
-                'width': '80%',
-                'margin': 'auto',
-                'paddingTop': '80%',
-                'backgroundColor': 'lime',
-                'borderRadius': '50%'
-              };
-              let cummDemand = 0;
-              let borderToggle = false;
-              previousDateLine = currentDateLine;
-
-              return (
-                <tr key={contract.id}>
-
-                  <td> {index + 1} </td>
-                  <td>
-                    <div style={riskStoplight} />
-                  </td>
-                  <td> {contract.partNumber} </td>
-                  <td> {contract.nomenclature} </td>
-                  <td> {contract.WIP} </td>
-                  <td> {contract.inventory} </td>
-
-                  {contract.demand.map(line => {
-                    // Red border coloring to show current date line
-                    let borderLeftWidth = 'thin';
-                    let borderTopWidth = 'thin';
-                    if (!borderToggle && moment(line.needDate).format('YYYY-MM-DD') >= moment().format('YYYY-MM-DD')) {
-                      borderLeftWidth = 'medium';
-                      borderToggle = true;
-                      currentDateLine = line.lineNumber;
-                    }
-                    if (index !== 0 && !borderToggle && line.lineNumber >= previousDateLine) {
-                      borderTopWidth = 'medium';
-                    } else if (index !== 0 && borderToggle && line.lineNumber < previousDateLine) {
-                      borderTopWidth = 'medium';
-                    }
-
-                    // Green background for demand that has inventory, yellow background for demand that has WIP
-                    cummDemand += line.qty;
-                    let backgroundColor = 'white';
-                    if (line.qty === 0) {
-                      backgroundColor = 'darkgray';
-                      line.qty = '';
-                    } else if (contract.inventory >= cummDemand) {
-                      backgroundColor = 'lime';
-                    } else if (contract.inventory + contract.WIP >= cummDemand) {
-                      // Orange background for past due demand that has WIP
-                      if (moment(line.needDate).format('YYYY-MM-DD') < moment().format('YYYY-MM-DD')) {
-                        backgroundColor = 'orange';
-                        riskStoplight.backgroundColor = 'orange';
-                      } else {
-                        backgroundColor = 'yellow';
-                      }
-                    } else if (moment(line.needDate).format('YYYY-MM-DD') < moment().format('YYYY-MM-DD')) {
-                      // Orange background for past due demand that has WIP
-                      backgroundColor = 'red';
-                      riskStoplight.backgroundColor = 'red';
-                    }
-                    return <td key={line.id} style={{'backgroundColor': backgroundColor, 'borderLeftWidth': borderLeftWidth, 'borderTopWidth': borderTopWidth}}> {line.qty} </td>
-                  })}
-
-                </tr>
-              );
-            })}
-          </tbody>
+          {demandMatrix}
 
         </table>
 
       </div>
-  );
+    );
+  };
 };
 
 export default LOB;
