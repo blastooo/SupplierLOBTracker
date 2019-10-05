a3860a2ff694d9d470bff66784f95ba7d64bc08f
James Lee <james.k.lee4@gmail.com>
2019-10-05 09:26:26 -0700

    Client: Remove 'Select' from dropdown after a supplier is selected

--- client/src/components/MainHeader.jsx
+++ client/src/components/MainHeader.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const MainHeader = ({supplierList, changeSupplier}) =>
+const MainHeader = ({supplierList, changeSupplier, supplierId}) =>
   <div>
 
     <h1> Supplier Line of Balance </h1>
 
     <label> Choose Supplier: </label>
     <select onChange={changeSupplier} >
-      <option> Select </option>
+      {!supplierId && <option> Select </option>}
       {supplierList.map(supplier =>
         <option key={supplier.id} value={supplier.id}> {supplier.supplierCode + ' ' + supplier.name} </option>
       )}
