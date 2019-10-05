a5ba264bb4328bc6513365d33b829510f07a9075
James Lee <james.k.lee4@gmail.com>
2019-10-05 09:29:27 -0700

    Add DemandMatrixByWeek component to display demand by week

---
+++ client/src/components/DemandMatrixByWeek.jsx
@@ -0,0 +1,91 @@
+import React from 'react';
+import moment from 'moment';
+
+const DemandMatrixByWeek = ({contracts, headers}) => {
+
+  return (
+    <tbody>
+      {contracts.map((contract, index) => {
+        let riskStoplight = {
+          'position': 'relative',
+          'height': 'auto%',
+          'width': '80%',
+          'margin': 'auto',
+          'paddingTop': '80%',
+          'backgroundColor': '#78FF78',
+          'borderRadius': '50%'
+        };
+        let cummDemand = 0;
+        let currentWeek = headers[0];
+        let borderToggle = false;
+        let i = 0;
+
+        return (
+          <tr key={contract.id}>
+
+            <td> {index + 1} </td>
+            <td>
+              <div style={riskStoplight} />
+            </td>
+            <td> {contract.partNumber} </td>
+            <td> {contract.nomenclature} </td>
+            <td> {contract.WIP} </td>
+            <td> {contract.inventory} </td>
+
+            {headers.map((date, index) => {
+              // Calculate demand for the week
+              let demand = 0;
+              for (; i < contract.demand.length; i++) {
+                let needDate = moment(contract.demand[i].needDate).format('YYYY-MM-DD');
+                if (needDate >= currentWeek && needDate < moment(currentWeek).add(1, 'week').format('YYYY-MM-DD')) {
+                  demand+= contract.demand[i].qty;
+                } else {
+                  currentWeek = moment(currentWeek).add(1, 'week').format('YYYY-MM-DD');
+                  break;
+                }
+              }
+
+              // Thick border coloring to show current date line
+              let borderLeftWidth = 'thin';
+
+              if (!borderToggle && moment(date).format('YYYY-MM-DD') >= moment().format('YYYY-MM-DD')) {
+                borderLeftWidth = 'medium';
+                borderToggle = true;
+              }
+
+              // Gray background for demand that has been fulfilled, green background for demand that has inventory, blue background for demand that has WIP
+              cummDemand += demand;
+
+              let color = 'black';
+              let backgroundColor = 'white';
+
+              if (demand === 0) {
+                color = 'darkgray'
+                backgroundColor = 'darkgray';
+              } else if (contract.inventory >= cummDemand) {
+                backgroundColor = '#78FF78';
+              } else if (contract.inventory + contract.WIP >= cummDemand) {
+                // Yellow background for past due demand that has WIP
+                if (moment(date).format('YYYY-MM-DD') < moment().format('YYYY-MM-DD')) {
+                  backgroundColor = '#FFFF69';
+                  riskStoplight.backgroundColor = '#FFFF69';
+                } else {
+                  backgroundColor = '#B8FFFF';
+                }
+              } else if (moment(date).format('YYYY-MM-DD') < moment().format('YYYY-MM-DD')) {
+                // Red background for past due demand that has no WIP
+                backgroundColor = '#FF3939';
+                riskStoplight.backgroundColor = '#FF3939';
+              }
+
+              return <td key={index} style={{'color': color, 'backgroundColor': backgroundColor, 'borderLeftWidth': borderLeftWidth}}> {demand} </td>
+            })}
+
+          </tr>
+        );
+      })}
+    </tbody>
+  );
+};
+
+export default DemandMatrixByWeek;
