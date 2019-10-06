d6224b6bd2c145ebbcd3d1603e785eb7c8eb38e4
James Lee <james.k.lee4@gmail.com>
2019-10-06 03:36:30 -0700

    Client: Add Legend component to explain color scheme

---
+++ client/src/components/Legend.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const Legend = () =>
+  <table style={{'borderCollapse': 'separate', 'borderRadius':'0.4em', 'padding': '0.5em'}}>
+    <thead>
+      <tr>
+        <th colSpan="2" style={{'fontSize': '1.07em', 'letterSpacing': '0.2em'}}> LEGEND </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr><td style={{'border': 'none'}}/></tr>
+      <tr>
+        <td style={{'border': 'none', 'width': '1.2em', 'backgroundColor': 'darkgray'}} />
+        <td style={{'padding': '0.3em', 'border': 'none', 'textAlign': 'left'}}> Inventory Allocated </td>
+      </tr>
+      <tr>
+        <td style={{'border': 'none', 'width': '1.2em', 'backgroundColor': '#78FF78'}} />
+        <td style={{'padding': '0.3em', 'border': 'none', 'textAlign': 'left'}}> Inventory Coverage </td>
+      </tr>
+      <tr>
+        <td style={{'border': 'none', 'width': '1.2em', 'backgroundColor': '#B8FFFF'}} />
+        <td style={{'padding': '0.3em', 'border': 'none', 'textAlign': 'left'}}> WIP Coverage </td>
+      </tr>
+      <tr>
+        <td style={{'border': 'none', 'width': '1.2em', 'backgroundColor': '#FFFF69'}} />
+        <td style={{'padding': '0.3em', 'border': 'none', 'textAlign': 'left'}}> Behind Schedule with WIP </td>
+      </tr>
+      <tr>
+        <td style={{'border': 'none', 'width': '1.2em', 'backgroundColor': '#FF3939'}} />
+        <td style={{'padding': '0.3em', 'border': 'none', 'textAlign': 'left'}}> Behind Schedule with no WIP </td>
+      </tr>
+    </tbody>
+  </table>
+
+export default Legend;
